1e6a621825586240f21875df65eb1aeeb77c1c29
ip11 <iprash011@gmail.com>
2018-08-02 14:48:51 +0530

    deleted sid's crap

--- mvc/index.js
+++ mvc/index.js
@@ -1,30 +1,15 @@
-const http = require("http");
+var http = require("http");
 
-http.createServer(function (req, res) {
-    // res.write('Hello World!'); 
-    res.end('Hello World!'); 
-    
-  }).listen(8080);
+http.createServer(function (request, response) {
 
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   console.log("Hai sara");
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
 
-// var server1 = http.createServer(function (req, res) {
-//     res.write('Hello World!'); 
-//     res.end(); 
-//   });
-// server1.listen(8080);
-
-//  http.createServer(function (req, res) {
-//     res.write('Hello World!'); 
-//     res.end(); 
-//   });
-
-// http.createServer().listen(8080);
-
-//  http.createServer(function (req, res) {
-//     res.write('Hello World!'); 
-//     res.end(); 
-//   });
-
-// http.createServer.listen(8080);
-
-
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
